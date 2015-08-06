2f5ef0c40362c4e5e6aaad3984e5d5f256b273b5
Varmin <varmin.wikipedia@gmail.com>
2015-08-06 13:39:34 -0400

    updated doc

---
+++ example1/scriptdot.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string.h>
+#include <regex.h>
+#include <ctype.h>
+
+#define taille 1000000
+
+using namespace std;
+
+
+char s[taille+1];
+
+int main(int argc, char ** argv)
+{
+    if (argc <= 1) printf("error not enough arguments\n");
+    else if (argc == 2) 
+    {
+	int state = 0;
+        //printf("opening %s\n", argv[1]);
+        FILE* f = fopen(argv[1],"r+");
+        if (f == NULL) {printf ("Error opening file"); exit(-1);}
+        while(fgets(s,taille, f) != NULL)
+        {
+	    if(strstr(s, "DotEnd*/") != NULL) state = 2;
+            if(state == 1) 
+            {
+                if(strstr(s,"?????") != NULL)
+                {   
+                    strtok(argv[1],"/");
+                    printf("%s%s",strtok (NULL, "/"),s+5);
+                }
+                else printf("%s",s);
+            }
+            if(strstr(s, "/*DotBegin") != NULL) state = 1;
+        }
+        fclose(f);
+        f = NULL;
+    }
+    else if (argc > 2) printf("error too many arguments\n"); 
+
+    return 0;
+}
